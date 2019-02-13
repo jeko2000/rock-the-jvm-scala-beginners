fc54438a5569865bb9e26f66d0a7f182831dd9e6
Johnny Ruiz <jeko2000@yandex.com>
2019-02-12 22:41:29 -0500

    Complete 20. Object-Oriented Exercises: Expanding Our Collection

--- src/main/scala/excercises/JMyList.scala
+++ src/main/scala/excercises/JMyList.scala
@@ -5,6 +5,10 @@ abstract class JMyList[+A] {
   def tail: JMyList[A]
   def isEmpty: Boolean
   def add[B >: A](b: B): JMyList[B]
+  def map[B](transformer: JMyTransformer[A,B]): JMyList[B]
+  def filter(predicate: JMyPredicate[A]): JMyList[A]
+  def concat[B >: A](list: JMyList[B]): JMyList[B]
+  def flatmap[B](transformer: JMyTransformer[A, JMyList[B]]): JMyList[B]
   def printElements: String
   override def toString: String = "[" + printElements + "]"
 }
@@ -14,6 +18,10 @@ object JEmpty extends JMyList[Nothing] {
   def tail: JMyList[Nothing] = throw new NoSuchElementException
   def isEmpty: Boolean = true
   def add[B >: Nothing](b: B): JMyList[B] = new JCons[B](b, this)
+  def map[B](transformer: JMyTransformer[Nothing,B]): JMyList[B] = JEmpty
+  def filter(predicate: JMyPredicate[Nothing]): JMyList[Nothing] = JEmpty
+  def concat[B >: Nothing](list: JMyList[B]): JMyList[B] = list
+  def flatmap[B](transformer: JMyTransformer[Nothing, JMyList[B]]): JMyList[B] = JEmpty
   def printElements: String = ""
 }
 
@@ -22,9 +30,26 @@ class JCons[+A](h: A, t: JMyList[A]) extends JMyList[A] {
   def tail: JMyList[A] = t
   def isEmpty: Boolean = false
   def add[B >: A](b: B): JMyList[B] = new JCons[B](b, this)
+  def map[B](transformer: JMyTransformer[A,B]): JMyList[B] =
+    new JCons(transformer.transform(h), t.map(transformer))
+  def filter(predicate: JMyPredicate[A]): JMyList[A] =
+    if (predicate.test(h)) new JCons(h, t.filter(predicate))
+    else t.filter(predicate)
+  def concat[B >: A](list: JMyList[B]): JMyList[B] =
+    new JCons(h, t.concat(list))
+  def flatmap[B](transformer: JMyTransformer[A, JMyList[B]]): JMyList[B] =
+    transformer.transform(h) concat t.flatmap(transformer)
   def printElements: String =
     if (t.isEmpty) "" + h
     else h.toString + " " + t.printElements
+}
+
+trait JMyPredicate[-A] {
+  def test(a: A): Boolean
+}
+
+trait JMyTransformer[-A, B]{
+  def transform(a: A): B
 }
 
 object JListTest {
@@ -39,5 +64,17 @@ object JListTest {
   println(listOfStrings.add("d").head)
   println(listOfStrings.isEmpty)
   println(listOfStrings.toString)
+
+  println(listOfInts.filter(new JMyPredicate[Int] {
+    override def test(n: Int) = n % 2 == 0
+  }))
+
+  println(listOfStrings.map(new JMyTransformer[String, String] {
+    override def transform(s: String) = s + s
+  }))
+
+  println(listOfStrings.flatmap(new JMyTransformer[String, JMyList[String]] {
+    override def transform(s: String) = JEmpty.add(s).add(s).add(s)
+  }))
 
 }
--- src/main/scala/lectures/part2oop/AnonymousClasses.scala
+++ src/main/scala/lectures/part2oop/AnonymousClasses.scala
@@ -29,4 +29,20 @@ object AnonymousClasses {
   val jim = new Person("Jim"){
     override def sayHi: Unit = println(s"Hi, my name is Jim, how can I be of service?")
   }
+
+  /*
+   1. Generic trait MyPredicate[-T] with a little method test(T) => Boolean
+   2. Generic trait MyTransformer[-A, B]
+   3. MyList:
+       - map(tranformer) => MyList
+       - filter(predicate) => MyList
+       - flatmap(transformer from A to MyList[B]) => MyList[B]
+
+       class EvenPredicate extends MyPredicate[Int]
+       class StringToIntTransformer extends MyTransformer[String, Int]
+
+       [1,2,3].map(n*2) = [2,4,6]
+       [1,2,3,4].filter(n%2) = [2,4]
+       [1,2,3,4].flatmap(n =? [n, n+1]) => [1,2,2,3,3,4]
+   */
 }
